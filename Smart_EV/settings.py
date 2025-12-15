0bd1b309b41e84c8e6409dfe3d7264d2dfbc71eb
Achali-dev <acsk22cs@cmrit.ac.in>
2025-12-16 00:08:04 +0530

    Update settings for Render deployment - use environment variables

--- Smart_EV/settings.py
+++ Smart_EV/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14cg5v%z8qe75=)7pnuinh%nhz_cm5_$gbjop35$3t%vys)qk5'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-14cg5v%z8qe75=)7pnuinh%nhz_cm5_$gbjop35$3t%vys)qk5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
